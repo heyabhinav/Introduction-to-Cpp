2bbbd1402386dc8aaf129bf4f33520b83af2609e
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2022-03-31 22:09:39 +0530

    First program, intro to 'cout', prints hello world.

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+/*Program to print hello world.
+
+Here 'iostream' header file is included because it provides input output streams which are cin and cout.
+In this program we'll see the use of 'cout'.
+-> int main() is a function were we write our code, and also this is the function which executes itself when we run our code.
+*/
+
+#include<iostream>
+using namespace std; 
+int main()
+{
+    cout << "Hello world!"; 
+    return 0;
+}
